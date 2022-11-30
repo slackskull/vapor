56f617b1f1637ea1979eeda8bd13be810237b0c1
Slackskull <lukas_discovery@hotmail.com>
2022-11-30 19:07:30 -0300

    UPDATE @ Adicionado comentarios da função do java script

--- painel/assets/scripts.js
+++ painel/assets/scripts.js
@@ -1,3 +1,6 @@
+
+// INICIO FUNÇÃO DE EXCLUIR JOGO
+
 function excluirJogo(idJogo) {
     let excluirJogo = confirm ("Realmete deseja excluir esse jogo?")
 
@@ -5,3 +8,9 @@ function excluirJogo(idJogo) {
         window.open("excluir-jogo.php?id=" + idJogo, "_SELF")
     }
 }
+
+// ESTA FUNÇÃO IRA ABRIR UMA JANELA DE CONFIRMAÇÃO SE VOCÊ REALMENTE DESEJA EXCLUIR O JOGO COM UM "OK" OU "CANCELAR".
+// AO APERTAR "OK" ELE VALIDA COMO TRUE E EXCLUE O JOGO COM UM if, AO APERTAR CANCELAR ELE FECHA A JANELA E NÃO FAZ NADA.
+
+
+// FIM FUNÇÃO DE EXCLUIR JOGO
