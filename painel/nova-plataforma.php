fd550aa3a9496caec5ea627aaae4c96cba02f69d
Slackskull <lukas_discovery@hotmail.com>
2022-12-06 21:35:49 -0300

    UPDATE @ Adicionado funcao de transformar primeira letra em maiuscula no nova-plataforma

--- painel/nova-plataforma.php
+++ painel/nova-plataforma.php
@@ -6,7 +6,7 @@
 <form action="cadastrar-plataforma.php" method="post">
     <div id="form-create">
         <p>Nova Plataforma</p>
-        <input type="text" name="platform" id="platform" placeholder="Plataforma" onclick="removeErrorMessageplataforma()" >
+        <input type="text" name="platform" id="platform" placeholder="Plataforma" onkeyup ="transformar_primeira_letra_maiuscula(this)" >
         <br></br>
         <button type="submit">Salvar</button>
     </div>
